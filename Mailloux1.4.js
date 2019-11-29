3622adf0373b5adb47b2b9cedf7a4c250dcb9878
Laurie Mailloux <llmailloux@comcast.net>
2019-11-28 19:07:50 -0500

    Adding assignment Mailloux1.4.js

---
+++ Mailloux1.4.js
@@ -0,0 +1,25 @@
+// First Name//
+var firstName = "Laurie";
+console.log (firstName);
+
+//Last Name//
+var lastName = "Mailloux";
+console.log (lastName);
+
+//Age//
+var age = "47";
+console.log (age);
+
+//Street Name//
+var street = "Birdseye Street";
+console.log (street);
+
+//City//
+var city = "Christmas";
+console.log (city);
+
+//Zip Code//
+var zipCode = "45825";
+console.log (zipCode);
+
+
