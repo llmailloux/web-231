b3e9eb56819c8e6e00ee5ff26a75adb5f16f4bfd
Laurie Mailloux <llmailloux@comcast.net>
2019-12-04 12:23:55 -0500

    Add files via upload

---
+++ Week-2/Mailloux-header-test.js
@@ -0,0 +1,4 @@
+const header = require('Laurie, Mailloux, Assignment 2.2/Mailloux-header.js');
+console.log(header.display("Laurie", "Mailloux", "Assignment 2.2"));
+
+
--- Week-2/Mailloux-header.js
+++ Week-2/Mailloux-header.js
@@ -1,1 +1,21 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso 
+; Date:   December 3, 2019
+; Modified By: Laurie Mailloux
+; Description: Displays a formatted header
+;===========================================
+*/
 
+/**
+* Params: firstName, lastName, assignment
+* Response: output 
+* Description: Returns a well-formatted string header
+*/
+exports.display = function (Laurie, Mailloux, week 2) {
+	let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' + 
+	new Date().toLocaleDateString('en-US')
+
+	return output  
+}
