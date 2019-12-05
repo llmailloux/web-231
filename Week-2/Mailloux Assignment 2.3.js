ff13b9f96f231c30e629d439f81a03b1b681fe8e
Laurie Mailloux <llmailloux@comcast.net>
2019-12-04 20:09:25 -0500

    Updating Week-2)

---
+++ Week-2/Mailloux Assignment 2.3.js
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title:  Mailloux Assignment2.3.js
+; Author: Professor Krasso
+; Date:   4 December 2019
+; Modified By: Laurie Mailloux
+; Description: demonstrate a 0 parameter function.
+;===========================================
+*/
+
+//start program
+
+/*Expected output:
+FirstName LastName
+Exercise 2.3
+Today's Date
+
+Hello FirstName LastName!
+*/
+
+const header = require('./Mailloux-header.js');
+
+console.log(header.display("Laurie", "Mailloux", "Assignment 2.3"));
+
+myName.laurie = "Laurie";
+
+//function
+function myName(){
+return myName.laurie;
+}
+
+//output
+console.log ("\n");
+
+console.log ("Hello "  + myName() + " Mailloux!");
